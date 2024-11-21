81188860247726bacc98d071317362c5a82b5d72
rodrigogovea <rodrigogovea@live.com.mx>
2024-11-21 15:45:32 -0600

    Se modifico la cadena de conexion

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const db = new Sequelize('postgresql://res_api_photoblog_user:sFf044Knd8Jtnrd8ZnaXbYt56dJasoTd@dpg-csvp5a1u0jms738b36vg-a.oregon-postgres.render.com/res_api_photoblog')
+const db = new Sequelize('postgresql://res_api_photoblog_user:sFf044Knd8Jtnrd8ZnaXbYt56dJasoTd@dpg-csvp5a1u0jms738b36vg-a.oregon-postgres.render.com/res_api_photoblog?ssl=true')
 
 export default db 
