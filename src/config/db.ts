984c6cdd63ed9ef4f5fb1a00f1ee50b4ff593b48
rodrigogovea <rodrigogovea@live.com.mx>
2024-11-21 15:55:57 -0600

    Se oculto la cadena de conexion

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const db = new Sequelize('postgresql://res_api_photoblog_user:sFf044Knd8Jtnrd8ZnaXbYt56dJasoTd@dpg-csvp5a1u0jms738b36vg-a.oregon-postgres.render.com/res_api_photoblog?ssl=true')
+const db = new Sequelize(process.env.DATABASE_URL!)
 
 export default db 
